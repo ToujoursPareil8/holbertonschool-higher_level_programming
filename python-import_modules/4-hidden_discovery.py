21a9e47a087e7968944a13fb4ce9afe82a7aa564
jason <12315@holbertonstudents.com>
2026-01-16 14:34:24 +0000

    added tri

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import hidden_4
 
 if __name__ == "__main__":
-    for name in sorted(dir(hidden_4)):
+    # Récupérer tous les noms définis dans le module
+    names = dir(hidden_4)
+    
+    # Trier par ordre alphabétique
+    names.sort()
+    
+    # Filtrer et afficher les noms qui ne commencent pas par "__"
+    for name in names:
         if not name.startswith("__"):
             print(name)
-
