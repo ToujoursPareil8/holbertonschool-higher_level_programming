409b2629fa7c31e39dddb66f5df5cc6356ee66a8
Jason <jasonaridy@gmail.com>
2026-01-16 14:17:53 +0100

    fix missing backslach

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-"""This module prints "Programming is like building a multilingual puzzle" to the console."""
-print('"Programming is like building a multilingual puzzle')
+print("\"Programming is like building a multilingual puzzle")
