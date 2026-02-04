8f3fa21dc4e35ed46f5a604cf2b959ba4969afcc
Jason <jasonaridy@gmail.com>
2026-02-04 12:59:26 +0100

    adding all tasks (empty) at once

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" Module that demonstrates attribute lookup in Python inheritance."""
+def lookup(obj):
+    """Returns a list of available attributes and methods of an object."""
+    return dir(obj)
