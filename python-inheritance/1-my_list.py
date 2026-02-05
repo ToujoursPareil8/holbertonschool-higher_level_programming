0fadd9d204c259ef1364ff84ebb9c872de38857a
Jason <jasonaridy@gmail.com>
2026-02-05 11:12:52 +0100

    fix: changed sorted(self

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
-
-"""
-    Module with class MyList
-"""
+""" Class MyList """
 
 
 class MyList(list):
-    """
-    MyList class that inherit from list
+    """ class MyList inherite from list """
 
-    Args:
-        list (list): list of int
-    """
     def print_sorted(self):
-        """
-        print_sorted sort the list and display the result
-        """
-        sorted_list = sorted(self)
-        print(sorted_list)
+        """ print sorted list """
+        print(sorted(self))
